8d7bdf6a95c00404c2c37db154fcdc79605225c1
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-07-28 02:25:31 +0545

    first commit

---
+++ src/tourData.js
@@ -0,0 +1,31 @@
+export const tourData = [
+  {
+    id: 1,
+    city: 'new york',
+    img: './img/newyork.jpeg',
+    name: 'new york bridge tour',
+    info: 'Spider sense ',
+  },
+  {
+    id: 2,
+    city: 'paris',
+    img: './img/paris.jpeg',
+    name: 'paris lights tour',
+    info: 'Pika , pika, pikachu',
+  },
+  {
+    id: 3,
+    city: 'london',
+    img: './img/london.jpeg',
+    name: 'london royal palace tour',
+    info:
+      'Pika super happy Pika super happy Pika super happy Pika super happy Pika super happy Pika super happy Pika super happy Pika super happy Pika super happy Pika super happy ',
+  },
+  {
+    id: 4,
+    city: 'tokyo',
+    img: './img/tokyo.jpeg',
+    name: 'tokyo sushi tour',
+    info: 'Pika super happy',
+  },
+];
